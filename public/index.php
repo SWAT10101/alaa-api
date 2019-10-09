c4d309b7fb342251c2c01a581af604c7df77fc8a
SWAT10101 <swat1994911@gmail.com>
2019-10-10 00:03:26 +0300

    add crate user and haveEmptyParameters

--- public/index.php
+++ public/index.php
@@ -3,24 +3,106 @@ use \Psr\Http\Message\ServerRequestInterface as Request;
 use \Psr\Http\Message\ResponseInterface as Response;
 
 require '../vendor/autoload.php';
-require '../include/DbConnect.php';
+require '../include/DbOperations.php';
 
 $app = new \Slim\App;
 
+/*
+endpoint: createuser
+paramenters: firstname, lastname, email, password, phone, block, building, floor, flat
+method: post
+*/
 
-$app->get('/', function (Request $request, Response $response, array $args) {
-    
-    $response->getBody()->write(" Hello world");
-
-    $db = new DbConnect;
-
-    if($db->connect() != null)
+$app->post('/createuser', function(Request $request, Response $response){
+    if(!haveEmptyParameters(array('firstname','lastname', 'email', 'password', 'phone', 'block', 'building', 'floor', 'flat' ), $response))
     {
-       echo 'Connection Successfull';
+        $request_data = $request->getParsedBody();
+        
+        $firstname = $request_data['firstname'];
+        $lastname = $request_data['lastname'];
+        $email = $request_data['email'];
+        $password = $request_data['password'];
+        $phone = $request_data['phone'];
+        $block = $request_data['block'];
+        $building = $request_data['building'];
+        $floor = $request_data['floor'];
+        $flat = $request_data['flat'];
+
+        $hase_password = password_hash($password, PASSWORD_DEFAULT);
+
+        $db = new DbOperations;
+
+        $result = $db->createUser($firstname, $lastname, $email, $hase_password, $phone, $block, $building, $floor, $flat);
+        
+        if($result == USER_CREATED)
+        {
+           $message = array();
+           $message['error'] = false;
+           $message['message'] = 'User created successfully';
+           $response->write(json_encode($message));
+           return $response
+                          ->withHeader('Content-type', 'application/json')
+                          ->withStatus(201);
+
+        }
+        else if($result == USER_FAILURE)
+        {
+            $message = array();
+           $message['error'] = true;
+           $message['message'] = 'Some error occurred';
+           $response->write(json_encode($message));
+           return $response
+                          ->withHeader('Content-type', 'application/json')
+                          ->withStatus(422);
+
+        }
+        else if($result == USER_EXISTS)
+        {
+
+            $message = array();
+           $message['error'] = false;
+           $message['message'] = 'User Already Exists';
+           $response->write(json_encode($message));
+           return $response
+                          ->withHeader('Content-type', 'application/json')
+                          ->withStatus(422);
+
+        }
+        return $response
+                          ->withHeader('Content-type', 'application/json')
+                          ->withStatus(422);
+
+
     }
 
-    return $response;
+
 });
+
+function haveEmptyParameters($required_params, $response){
+
+    $error = false;
+    $error_params = '';
+    $request_params = $_REQUEST;
+
+    foreach($required_params as $param){
+        if(!isset($request_params[$param]) || strlen($request_params[$param]) <= 0)
+        {
+            $error = true;
+            $error_params .= $param . ', ';
+
+        }
+    }
+
+    if($error)
+    {
+        $error_detail = array();
+        $error_detail['error'] = true;
+        $error_detail['message'] = 'Required parameters ' . substr($error_params, 0, -2) . ' are missing or empty';
+        $response->write(json_encode($error_detail));
+    }
+    return $error;
+}
+
 
 
 
